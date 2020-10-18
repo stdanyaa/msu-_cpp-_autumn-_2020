6cd1026302257da7ba2aa9f045d70042d0f681a4
stdanyaa <58135183+stdanyaa@users.noreply.github.com>
2020-10-18 18:43:55 +0300

    Update Allocator.cpp

--- 01/Allocator.cpp
+++ 01/Allocator.cpp
@@ -2,23 +2,29 @@
 
 class Allocator{
     char* memory_pointer_;
-    size_t size_;
+    size_t size_ = -1;
     size_t offset_;
+
 
 public:
     void makeAllocator(size_t maxSize){
+        if (size_ != -1) return;
+        if (maxSize <= 0) return;
         size_ = maxSize;
         offset_ = 0;
         memory_pointer_ = new char[size_];
     };
     char* alloc(size_t size){
+        if (size_ == -1){
+            return nullptr;
+        }
         if (offset_ + size > size_){
             return nullptr;
         }
         if (size <= 0){
             return nullptr;
         }
-        char *ret = memory_pointer_ + offset_;
+        char* ret = memory_pointer_ + offset_;
         offset_ += size;
         return ret;
     };
